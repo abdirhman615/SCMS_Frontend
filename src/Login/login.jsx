8907f4faea6b26b29a57de713b90fb01f6938357
abdirhman615 <abdirhman615@gmail.com>
2024-02-03 01:26:04 +0300

    test

--- src/Login/login.jsx
+++ src/Login/login.jsx
@@ -65,8 +65,8 @@ const {setIsLogin}= useUserContext();
 const {mutateAsync,isLoading,isError,error,data:response}= useMutation({
     mutationFn: async (data) => {
        // return await axios.post ('http://localhost:5000/login',data)
-        return await axios.post ('https://backend-scms.vercel.app/login',data)
-        // return await axios.post ('https://betahousebackendapi.vercel.app/login',data)
+       // return await axios.post ('https://backend-scms.vercel.app/login',data)
+         return await axios.post ('https://backend-scms.vercel.app/login',data)
 
     },
     onError:(err)=>{
